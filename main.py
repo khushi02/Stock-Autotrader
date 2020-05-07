3fd7fba7dc96dc1fc6d789642d9b618bdf95b3e8
khushi02 <k9wadhwa@gmail.com>
2020-05-07 16:55:56 -0700

    get S&P 500

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-import datetime as dt
-import matplotlib.pyplot as plt
-from matplotlib import style
-from mplfinance.original_flavor import candlestick_ohlc
-import matplotlib.dates as mdates
-import pandas as pd
-import pandas_datareader.data as web
+import bs4 as bs
+import pickle
+import requests
 
-style.use ('ggplot')
+def save_sp500_tickers():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'id': 'constituents'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.find('td').text.replace('\n','')
+        tickers.append(ticker)
+        
+    with open('sp500tickers.pickle', 'wb') as f:
+        pickle.dump(tickers, f)
+        
+    print(tickers)
+    return tickers
 
-## Create CSV
-#start = dt.datetime(2000, 1, 1)
-#end = dt.datetime(2016, 12, 31)
+save_sp500_tickers()    
 
-#df = web.DataReader('TSLA', 'yahoo', start, end)
-#df.to_csv('tsla.csv')
-
-df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
-#df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-
-df_ohlc = df['Adj Close'].resample('10D').ohlc()
-df_volume = df['Volume'].resample('10D').sum()
-
-df_ohlc.reset_index(inplace=True)
-df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
-#print(df_ohlc.head())
-
-ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
-ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
-ax1.xaxis_date()
-
-candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
-ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
-
-plt.show()

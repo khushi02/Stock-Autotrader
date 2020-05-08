14c4afc76bcf4d66edb1ec4a995852a1e46101e7
khushi02 <k9wadhwa@gmail.com>
2020-05-08 13:24:47 -0700

    compile data

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas_datareader.data as web
 import pickle
 import requests
 
+# Save company data in a csv file
 def save_sp500_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
@@ -22,9 +23,8 @@ def save_sp500_tickers():
         
     print(tickers)    
     return tickers
-
-save_sp500_tickers()    
-
+ 
+# Get S&P 500 company data
 def get_data_from_yahoo(reload_sp500=False):
     if reload_sp500:
         tickers = save_sp500_tickers()
@@ -46,4 +46,28 @@ def get_data_from_yahoo(reload_sp500=False):
         else:
             print('Already have {}'.format(ticker))
             
-get_data_from_yahoo()
+def compile_data():
+    with open('sp500tickers.pickle', 'rb') as f:
+        tickers = pickle.load(f)
+        
+    main_df = pd.DataFrame()
+    
+    for count,ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+        
+        df.rename(columns = {'Adj Close': ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+        
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+            
+        if count % 10 == 0:
+            print(count)
+            
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+    
+compile_data()

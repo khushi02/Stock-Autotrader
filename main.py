b47af80cc2cc7ac3f5458fc42e3ebc55d0c0fb10
khushi02 <k9wadhwa@gmail.com>
2020-05-08 11:05:07 -0700

    Get data

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
 import pickle
 import requests
 
@@ -9,13 +13,37 @@ def save_sp500_tickers():
     tickers = []
     for row in table.findAll('tr')[1:]:
         ticker = row.find('td').text.replace('\n','')
+        if "." in ticker:
+            ticker = ticker.replace('.','-')
         tickers.append(ticker)
         
     with open('sp500tickers.pickle', 'wb') as f:
         pickle.dump(tickers, f)
         
-    print(tickers)
+    print(tickers)    
     return tickers
 
 save_sp500_tickers()    
 
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open('sp500tickers.pickle', 'rb') as f:
+            tickers = pickle.load(f)
+            
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+        
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2020, 5, 7)
+    
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+            
+get_data_from_yahoo()

ab9197bd71e6c198a5a6ba5ec7094bd14cdae274
khushi02 <k9wadhwa@gmail.com>
2020-05-09 10:24:08 -0700

    create helper function

--- ml.py
+++ ml.py
@@ -9,10 +9,17 @@ def process_data_for_labels(ticker):
     df.fillna(0,inplace=True)
     
     for i in range(1, days+1):
-        df['{}_{}d'.format(ticker, i)] = ((df[ticker].shift(-i) - df[ticker]) / df[ticker]) * 100
+        df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
         
     df.fillna(0, inplace=True)
     return tickers, df
 
 process_data_for_labels('XOM')
         
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    req = 0.02
+    for col in cols:
+        if col > req:
+            return 1
+        else:

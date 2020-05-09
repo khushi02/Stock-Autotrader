60f892a445b1295405a31ec562ebcd18649b5bb9
khushi02 <k9wadhwa@gmail.com>
2020-05-09 10:52:43 -0700

    create featuresets

--- ml.py
+++ ml.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pickle
+from collections import Counter
 
 def process_data_for_labels(ticker):
     days = 7
@@ -12,9 +13,8 @@ def process_data_for_labels(ticker):
         df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
         
     df.fillna(0, inplace=True)
-    return tickers, df
+    return tickers, df, days
 
-process_data_for_labels('XOM')
         
 def buy_sell_hold(*args):
     cols = [c for c in args]
@@ -22,4 +22,30 @@ def buy_sell_hold(*args):
     for col in cols:
         if col > req:
             return 1
-        else:
+        elif col < -req:
+            return -1
+    return 0
+
+
+def extract_featuresets(ticker):
+    tickers, df, days = process_data_for_labels(ticker)
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold, 
+                                              *[df['{}_{}d'.format(ticker, i)]for i in range(1, days+1)]))
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data Spread: ', Counter(str_vals))
+    
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+    
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+    
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+    
+    return X, y, df
+
+extract_featuresets('XOM')
